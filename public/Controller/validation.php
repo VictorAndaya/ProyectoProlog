a37338e33afb08d36a0e20a8e19848fe90ed17b5
ArturoEsp <55172579+ArturoEsp@users.noreply.github.com>
2020-07-14 17:50:35 -0500

    Agrego los archivos principales
    Archivos principales y acceso login

---
+++ public/Controller/validation.php
@@ -0,0 +1,52 @@
+<?
+
+function AccessLogin($request){
+
+    $login=new Login();
+    return $login->AccessLogin($request);
+}
+
+class Login{
+
+    private $conexion;
+    
+    function __construct(){            
+        $database=new DbConnect();
+        $this->conexion=$database->connect();
+    }
+
+    function AccessLogin($request){
+
+        $data=json_decode($request->getbody());
+        $usuario = $data->usuario;
+        $contrasena = $data->contrasena;
+        $response;
+        $sql = "SELECT * FROM Usuarios WHERE Usuario = :usuario AND Contrasena = :contrasena";
+        try{   
+
+            $statement=$this->conexion->prepare($sql);
+            $statement->bindParam(":usuario",$usuario);
+            $statement->bindParam(":contrasena",$contrasena);
+            $statement->execute();
+            $count=$statement->rowCount();
+            
+            if($count)
+            {
+                $response->mensaje="Acceso correcto";
+            }
+            else
+            {
+                $response->mensaje="Usuario/Contrasena incorrecta";
+            }
+              
+
+        }catch(Exception $e){
+            $response=$e;
+        }
+        
+        return json_encode($response);
+    }
+
+    
+
+}
