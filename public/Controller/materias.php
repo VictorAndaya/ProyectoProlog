43c363f42afeee2c885aeb1c949cc75bddf7af29
Victor Andaya <48934786+VictorAndaya@users.noreply.github.com>
2020-07-17 16:46:54 -0500

    funciones update y delete

--- public/Controller/materias.php
+++ public/Controller/materias.php
@@ -7,6 +7,14 @@ function getmaterias($request){
     $materias=new Materia();
     return $materias->getMaterias($request);
 }
+function updatematerias($request){
+    $materias=new Materia();
+    return $materias->updateMaterias($request);
+}
+function deletematerias($request){
+    $materias=new Materia();
+    return $materias->deleteMaterias($request);
+}
 class Materia{
 
     private $conexion;
@@ -39,6 +47,39 @@ class Materia{
             $statement=$this->conexion->prepare($sql);            
             $statement->execute();
             $response=$statement->fetchall(PDO::FETCH_OBJ);            
+        }catch(Exception $e){
+            $response=$e;
+        }
+        return json_encode($response);
+    }
+
+    function updateMaterias($request){
+        $carreras;
+        $response;
+        $carrera=json_decode($request->getBody());
+        $sql="UPDATE Materias SET Nombre=:Nombre WHERE IdMateria=:IdMateria";    
+        try{            
+            $statement=$this->conexion->prepare($sql);
+            $statement->bindParam("IdMateria",$carrera->IdMateria);
+            $statement->bindParam("Nombre",$carrera->Nombre);
+            $statement->execute();
+            $response->mensaje="La Materia se actualizo correctamente";
+        }catch(Exception $e){
+            $response=$e;
+        }
+        return json_encode($response);
+    }
+
+    function deleteMaterias($request){
+        $carreras;
+        $response;
+        $carrera=json_decode($request->getBody());
+        $sql="DELETE FROM Materias WHERE IdMateria=:IdMateria";    
+        try{            
+            $statement=$this->conexion->prepare($sql);
+            $statement->bindParam("IdMateria",$carrera->IdMateria);
+            $statement->execute();
+            $response->mensaje="La Materia se elimino Correctamente";
         }catch(Exception $e){
             $response=$e;
         }
